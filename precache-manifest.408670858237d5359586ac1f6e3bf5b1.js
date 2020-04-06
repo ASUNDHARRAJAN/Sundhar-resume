71c25b07434f409a3b62957c7ac233b5d16335ef
ASUNDHARRAJAN <SundharAnnadurai@gmail.com>
2020-04-06 13:55:16 +0530

    Updates

--- precache-manifest.408670858237d5359586ac1f6e3bf5b1.js
+++ precache-manifest.408670858237d5359586ac1f6e3bf5b1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b6d9256a285bbdd6dc855d9d9c9ff020",
+    "revision": "a50ebc2d3860ab9ff5a9d340c8c253f5",
     "url": "/Sundhar-resume/index.html"
   },
   {
-    "revision": "f8cf7ef4bcffe8c2ef9d",
+    "revision": "fdfb0110f853258130cd",
     "url": "/Sundhar-resume/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "90e530a8586c322f54fa",
+    "revision": "c5ca26143c1775d3fe09",
     "url": "/Sundhar-resume/static/css/main.b4f03f54.chunk.css"
   },
   {
-    "revision": "f8cf7ef4bcffe8c2ef9d",
-    "url": "/Sundhar-resume/static/js/2.69fe4680.chunk.js"
+    "revision": "fdfb0110f853258130cd",
+    "url": "/Sundhar-resume/static/js/2.6cc1d82b.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/Sundhar-resume/static/js/2.69fe4680.chunk.js.LICENSE.txt"
+    "url": "/Sundhar-resume/static/js/2.6cc1d82b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "90e530a8586c322f54fa",
-    "url": "/Sundhar-resume/static/js/main.f0927e14.chunk.js"
+    "revision": "c5ca26143c1775d3fe09",
+    "url": "/Sundhar-resume/static/js/main.588f464a.chunk.js"
   },
   {
     "revision": "8da03f63cfebad86983c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sundhar-resume/precache-manifest.dafcf166b294ffa3814d0fc7e71f9b35.js"
+  "/Sundhar-resume/precache-manifest.408670858237d5359586ac1f6e3bf5b1.js"
 );
 
 self.addEventListener('message', (event) => {
