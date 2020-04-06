b17dae84959892d40fea784e40e6dd0ed7a9c341
ASUNDHARRAJAN <SundharAnnadurai@gmail.com>
2020-04-06 14:23:39 +0530

    REACT

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <HashRouter basename="/">
         <div>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-             <Link class="navbar-brand" to="/PROFILE">SUNDHARRAJAN</Link> 
+            <b>SUNDHARRAJAN</b>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
