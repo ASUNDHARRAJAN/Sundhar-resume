5f8093eb23c168595d0a6f739d961610227912de
ASUNDHARRAJAN <SundharAnnadurai@gmail.com>
2020-04-06 13:04:43 +0530

    REACTJS

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ render(){
     
     <Router>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="/Sundhar-resume/">SUNDHARRAJAN</a>
+      <a class="navbar-brand" href="/">SUNDHARRAJAN</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -18,7 +18,7 @@ render(){
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="/Sundhar-resume/">PROFILE</a>
+          <a class="nav-link" href="/">PROFILE</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/Sundhar-resume/Experience">EXPERIENCE</a>
