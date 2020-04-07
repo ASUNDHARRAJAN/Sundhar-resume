969bf844ba0fa2e918da280bf337e6db2f46026c
ASUNDHARRAJAN <SundharAnnadurai@gmail.com>
2020-04-07 13:03:26 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -41,12 +41,12 @@ const PROFILE = () =>
       <Image src="Profile.jpg" width="240" height="275" roundedCircle/>
     </div>
     <div class="col-md-6 profile_section_right">
-        <p><b>SUNDHARRAJAN </b> <br></br> SALEM,Tamilnadu <br></br> 8344230573 <br></br> SundharAnnadurai@gmail.com <br></br></p>
+        <p><b>SUNDHARRAJAN </b> <br></br> SALEM,TAMILNADU <br></br> 8344230573 <br></br> SundharAnnadurai@gmail.com <br></br></p>
     </div>
 </div><br></br>
 <div class="row objective_section">
   <div class="col-md-12">
-      <h4>OBJECTIIVE </h4>           
+      <h4>OBJECTIVE </h4>           
   </div>
 </div>
 <div class="row">
@@ -84,7 +84,7 @@ const PROFILE = () =>
 
 <h4>SKILLS</h4>
 <section class="Skills_section"><br></br>
-<h6 style={{fontSize:"1.15rem"}}>WEB DEVELOPMENT :</h6>
+<h6 style={{fontSize:"1.15rem"}}>FRONTEND TECHNOLOGIES</h6>
 <div class="row">
 <div class="col-md-1"></div>
 <div class="col-md-5"><p>HTML5</p></div>
@@ -156,7 +156,7 @@ const PROFILE = () =>
 </div>
 </div>
 </div><br></br>
-<h6 style={{fontSize:"1.15rem"}}>DATABASE :</h6>
+<h6 style={{fontSize:"1.15rem"}}>BACKEND TECHNOLOGIES</h6>
 <div class="row">
 <div class="col-md-1"></div>
 <div class="col-md-5"><p>MySQL</p></div>
@@ -190,7 +190,7 @@ const PROFILE = () =>
 </div>
 </div>
 </div><br></br>
-<h6 style={{fontSize:"1.15rem"}}>SAP CLOUD APPLICATION STUDIO(SDK) :</h6>
+<h6 style={{fontSize:"1.15rem"}}>SAP CLOUD APPLICATION STUDIO(SDK)</h6>
 <div class="row">
 <div class="col-md-1"></div>
 <div class="col-md-5"><p>BO, XBO, Screens, Process Extension Scenario and Adobe Live Cycle Designer – Forms Development</p></div>
@@ -208,37 +208,36 @@ const PROFILE = () =>
 
 
 
-
-
 const EXPERIENCE = () => 
 <section class="experience_background">
                 <div class="container">
                 <div class="col-md-12">
                   <br></br><h4>WORK EXPERIENCE</h4>
                 </div><br></br>
+                <div class="container">
                  <table class="table">
                       <tbody>
                         <tr>
-                          <td>December 2019 to Current</td>
+                          <td>December 2019 to Present</td>
                           <td><b>Web Developer</b><br></br>
-                          SearchNScore Solutions Coimbatore,Tamil Nadu</td>                      
+                          SearchNScore Solutions Coimbatore, Tamilnadu.</td>                      
                         </tr>
                         <tr>
                           <td>September 2018 to November 2019</td>
                           <td><b>SAP Business ByDesign Consultant and Web Developer</b><br></br>
-                          Nipurna IT Solutions Private Limited Salem, Tamil Nadu.</td>                        
+                          Nipurna IT Solutions Private Limited Salem, Tamilnadu.</td>                        
                         </tr>
                         <tr>
                           <td>March 2018 to September 2018</td>
-                          <td><b>Web Developer</b><br></br>Grosil,Salem, Tamilnadu</td>
+                          <td><b>Web Developer</b><br></br>Grosil Salem, Tamilnadu.</td>
                         </tr>
                       </tbody>
-                  </table>
+                  </table></div>
                   </div>
                   <div class="container cc_list">
                   <h4>CERTIFICATION COURSES</h4><br></br>
                   <ul class="container">
-                    <li>Web Developer Boot Camp (<b>HTML5, Bootstrap4, CSS3, Java Script, Jquery</b>) – Udemy</li>
+                    <li>Web Developer Boot Camp (<b>HTML5, Bootstrap4, CSS3, JavaScript, JQuery</b>) – Udemy</li>
                     <li>Attended training course in “<b>C</b>” programming Language conducted by Canada India Institutional Co – operational Project.</li>
                     <li>Completed course in “<b>Graphics Designer</b>” in Image Institute at Salem.</li>
                   </ul>
@@ -252,22 +251,22 @@ const PROJECTS = () =>
               <div class="container">
                   <br></br><h5>WEB DEVELOPMENT PROJECTS</h5><br></br>
                   
-                    <h6><b>Anna Land Developers (HTML5, CSS3, Javascript, Jquery and Boostrap):</b></h6>
+                    <h6><b>Anna Land Developers (HTML5, CSS3, JavaScript, JQuery and Bootstrap4) :</b></h6>
                     <div class="container">
                   <ul>
-                    <li>The Website provides functionalities to manage various modules as project module, services,contact, team and etc.</li>
-                    <li>Covert <b>PSD TO HTML5</b></li>
+                    <li>The Website provides functionalities to manage various modules as project module, services, contact, team and etc.</li>
+                    <li>Convert <b>PSD to HTML5</b></li>
                   </ul><br></br>
                   </div>
-                  <h5>SAP BUSINESS BY DESIGN CUSTOMIZED PROJECTS:</h5><br></br>
+                  <h5>SAP BUSINESS BY DESIGN CUSTOMIZED PROJECTS :</h5><br></br>
                   <p>
                       <b>SAP C4C & ByD Integrations:</b><br></br><br></br>
                       <b>Customization 1:</b> Real time integration of Account Master and associated objects (Attachments and Sales activities, Involved parties) from C4C to ByD. Custom program was written at C4C to consume the SAP ByDstandard account master interface.<br></br>
-                      <b>Customization 2:</b> Real time integration of Contact master from ByD to C4C, Custom program was written at C4C to consume the SAP ByD standard Contact master Web service.<br></br>
-                      <b>Customization 3:</b>SAP ByD invoice requests are integrated with C4C invoice request. Sales users can trigger the release of invoice request from C4C to ByD via the custom interface.
+                      <b>Customization 2:</b> Real time integration of Contact Master from ByD to C4C, Custom program was written at C4C to consume the SAP ByD standard Contact Master Web service.<br></br>
+                      <b>Customization 3:</b> SAP ByD invoice requests are integrated with C4C invoice request. Sales users can trigger the release of invoice request from C4C to ByD via the custom interface.
                   </p>
                   <p>
-                    <b>SAPByD – Selection of Identified Stock During Sales Quotation Process:</b><br></br><br></br>
+                    <b>SAPByD – Selection of Identified Stock During Sales Quotation Process :</b><br></br><br></br>
                     <div class="container">
                     <ul>
                     <li><b>SAP ByD Customization -</b> Selection of Identified Stock during Sales Quotation and Sales Order Process and it is extended till outbound delivery.</li>
@@ -275,15 +274,15 @@ const PROJECTS = () =>
                     </div>
                   </p> 
                   <p>
-                    <b>Form Development:</b>
+                    <b>Form Development :</b>
                     <div class="container">
                       <ul>
-                        <li>Designing the Form for Meeting Report,Production Specification,Performa Invoice,Sales Order,Purchase Order etc.</li>
+                        <li>Designing the Form for Meeting Report, Production Specification, Performa Invoice, Sales Order, Purchase Order etc.</li>
                       </ul>
                     </div>
                   </p>
                   <p>
-                    <b>Freight List- MES Integration Custom Developments:</b>
+                    <b>Freight List- MES Integration Custom Developments :</b>
                     <div class="container">
                       <ul>
                       <li>Custom Interface/ Webservice built to integrate MES system and Freight List of SAP and This Webservice captures all the information of shipping like Product, Pack Quantity, Trailer No. etc.</li>
